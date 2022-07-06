eadc0a0d3cd48941e5fafdcae883b8f0b51842b7
JakoDel <jacopodelvivo@gmail.com>
2022-07-06 13:47:05 +0200

    yet another fuckup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,5 +2,3 @@ LOCAL_PATH := device/samsung/j5xnlte
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_j5xnlte.mk
 LOCAL_PATH := device/samsung/j5xnlte
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_j5xnlte.mk
